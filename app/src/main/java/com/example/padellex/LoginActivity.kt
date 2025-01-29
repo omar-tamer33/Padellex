96831f31b262b2fc8e15c5fd706c8a1c8af51d3b
OMAR <omartamer5535002@gmail.com>
2025-01-29 13:41:41 +0200

    + Added LoginActivity.kt with its functions + Added SignupActivity.kt

--- app/src/main/java/com/example/padellex/LoginActivity.kt
+++ app/src/main/java/com/example/padellex/LoginActivity.kt
@@ -1,20 +1,68 @@
 package com.example.padellex
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.padellex.databinding.ActivityLoginBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding.loginBtn.setOnClickListener {
+            userLoginWithEmailAndPassword()
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkUserLoginState()
+    }
+
+    private fun checkUserLoginState() {
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val intent = Intent(this,SignupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun userLoginWithEmailAndPassword(){
+        val email = binding.emailEt.text.toString()
+        val password = binding.passwordEt.text.toString()
+        if (email.isNotBlank() && password.isNotBlank()){
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    auth.signInWithEmailAndPassword(email,password).await()
+                    checkUserLoginState()
+                }catch (e: Exception){
+                    withContext(Dispatchers.Main){
+                        binding.email.error = getString(R.string.error)
+                        binding.password.error = getString(R.string.error)
+                    }
+                }
+            }
         }
     }
 }
